a4f08c1df26da77bda78bb6294595f2e4e8c4db8
jzlin <jerry@114-43-131-8.dynamic.hinet.net>
2013-11-02 00:54:38 +0800

    fixed bug
    fixed logic bug

--- commandsListener.js
+++ commandsListener.js
@@ -5,7 +5,7 @@ chrome.commands.onCommand.addListener(function(command)
 	console.debug('command is : ' + command);
 
 	// Develop use : reload extension.
-	if (command = 'reload_extension')
+	if (command == 'reload_extension')
 		chrome.runtime.reload();
 });
 
